da43d49037563064bcffc39f00d5173f78ae012f
Narimbetov00 <narimbetov00.mail.ru>
2024-06-27 11:46:47 +0500

    staticfiles qosildi

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage,name='homepage'),
+    path('login/',views.loginpage,name='login'),
     path('post/<int:id>/',views.post_detail,name='post_detail'),
     path('post/<int:id>/update',views.update_post,name='update_post'),
     path('post/<int:id>/delete',views.delete_post,name='delete_post'),
